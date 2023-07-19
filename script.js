d4cced2dbe4af3a33e01036daf5a4ce994132b66
Jose Laurito <jigolaurito93@yahoo.com>
2023-07-19 03:11:29 -0500

    attached pokeapi using json

--- script.js
+++ script.js
@@ -16,3 +16,40 @@ const colors = {
     fighting: '#E6E0D4',
     normal: '#F5F5F5'
 }
+
+const fetchPokemons = async () => {
+    for(let i = 1; i <= pokemon_count; i++) {
+        await getPokemon(i)
+    }
+}
+
+const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    createPokemonCard(data)
+}
+
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+
+    const pokemonInnerHTML = `
+        <div class="img-container">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png" alt="">
+        </div>
+        <div class="info">
+            <span class="number">#001</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type: <span>grass</span></small>
+        </div>
+            `
+
+        pokemonEl.innerHTML = pokemonInnerHTML
+
+        poke_container.appendChild(pokemonEl)
+}
+
+fetchPokemons()
